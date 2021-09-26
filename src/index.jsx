d8046fc086e7d2fb6f8917b20608b544e274c37f
neiderbtc <neiderbtc@gmail.com>
2021-09-26 18:02:19 -0500

    add css bootstrap

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,5 @@
 import { render } from "solid-js/web";
-
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from "./App";
 
 render(App, document.getElementById("root"));
